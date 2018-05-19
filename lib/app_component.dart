4e41752c14b1f18a4e94c3afa884a65965531f94
Ram Manor <ram.manor.rm@gmail.com>
2018-05-19 21:46:21 +0300

    finished Hero app tutorial step 2

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,24 +1,18 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
+import 'src/hero.dart';
+import 'src/mock_heros.dart';
+
 @Component(
   selector: 'my-app',
-  template: '''
-    <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
-  ''',
-  directives: const [formDirectives],
+  templateUrl: 'app_component.html',
+  styleUrls: const ['app_component.css'],
+  directives: const [CORE_DIRECTIVES, formDirectives],
 )
 class AppComponent {
   final title = 'Tour of Heroes';
-  Hero hero = new Hero(1, 'Windstorm');
-}
-class Hero {
-  final int id;
-  String name;
-  Hero(this.id, this.name);
+  Hero selectedHero;
+  List<Hero> heroes = mockHeroes;
+
+  void onSelect(Hero hero) => selectedHero = hero;
 }
