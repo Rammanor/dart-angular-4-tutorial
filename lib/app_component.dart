5a0e7ac514ed8e89d45165367fc78cdc1c6d202b
Ram Manor <ram.manor.rm@gmail.com>
2018-05-19 21:35:17 +0300

    finished Hero app tutorial step 1

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,24 @@
 import 'package:angular/angular.dart';
-
+import 'package:angular_forms/angular_forms.dart';
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  template: '''
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+  ''',
+  directives: const [formDirectives],
 )
 class AppComponent {
-  var name = 'Angular';
+  final title = 'Tour of Heroes';
+  Hero hero = new Hero(1, 'Windstorm');
+}
+class Hero {
+  final int id;
+  String name;
+  Hero(this.id, this.name);
 }
