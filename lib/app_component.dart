44617de3237ba75cd3cfcfc503f1506116bbb0bb
Ram Manor <ram.manor.rm@gmail.com>
2018-05-20 09:44:45 +0300

    finished Hero app tutorial step 4

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,20 +1,32 @@
+import 'dart:async';
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 import 'src/hero.dart';
 import 'src/hero_detail_component/hero_detail_component.dart';
-import 'src/mock_heros.dart';
+import 'src/hero_service.dart';
+import 'src/mock_heroes.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
   styleUrls: const ['app_component.css'],
   directives: const [CORE_DIRECTIVES, formDirectives, HeroDetailComponent],
+  providers: const [HeroService],
 )
-class AppComponent {
+class AppComponent implements OnInit {
   final name = 'Angular';
   final title = 'Tour of Heroes';
   Hero selectedHero;
   List<Hero> heroes = mockHeroes;
+  final HeroService _heroService;
+  
+  AppComponent(this._heroService);
+
+  void ngOnInit() => getHeroes();
 
   void onSelect(Hero hero) => selectedHero = hero;
+
+  Future<Null> getHeroes() async {
+    heroes = await _heroService.getHeroes();
+  }
 }
---
+++ lib/src/hero_service.dart
@@ -0,0 +1,10 @@
+import 'dart:async';
+import 'package:angular/angular.dart';
+
+import 'hero.dart';
+import 'mock_heroes.dart';
+
+@Injectable()
+class HeroService {
+  Future<List<Hero>> getHeroes() async => mockHeroes;
+}
